f90f7cee0ff342cb406d588dbad9d6e037f7cb58
Sebastião Silva <swillian2009@gmail.com>
2022-01-06 08:52:03 -0300

    Inclusão de propriedades das matérias.

--- Models/Materia.cs
+++ Models/Materia.cs
@@ -1,7 +1,19 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace MongoDbExample.Models
 {
     public class Materia
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+    
+        /// <summary></summary>
+        public string Name { get; set; }
+        
+        /// <summary></summary>
+        public string Description { get; set; }
         
     }
 }
